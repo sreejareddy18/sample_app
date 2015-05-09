019b4ae92cf3ee31cf851385fd8c18b079b71987
Sreeja Reddy <Sreeja.Reddy@Cerner.com>
2015-05-09 07:32:51 +0530

    Finish static pages

--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
-
+require 'spec_helper'
+require 'capybara/rspec'
 # Specs in this file have access to a helper object that includes
 # the StaticPagesHelper. For example:
 #
